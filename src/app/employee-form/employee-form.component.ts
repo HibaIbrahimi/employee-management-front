20a24b61dd57787e293ac7ab2059774df73eec5e
HIBA <ibrahimih.hiba@gmail.com>
2026-01-04 16:51:43 +0100

    Complete employee CRUD functionality

--- src/app/employee-form/employee-form.component.ts
+++ src/app/employee-form/employee-form.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../employee.service';
-import { Router } from '@angular/router';
+import { Router , ActivatedRoute} from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,7 +12,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css'
 })
-export class EmployeeFormComponent {
+export class EmployeeFormComponent implements OnInit {
 
   employee: Employee = {
     id: 0,
@@ -23,20 +23,59 @@ export class EmployeeFormComponent {
     position: ''
   }
 
+  isEditing: boolean = false;
+
   errorMessage = "";
-  constructor(private employeeService: EmployeeService, private router: Router) {}
+  constructor(
+    private employeeService: EmployeeService, 
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+        this.route.paramMap.subscribe((result) => {
+
+          const id = result.get('id');
+
+          if(id){
+            // editing employee
+            this.isEditing = true;
+            
+            this.employeeService.getEmployeeById(Number(id)).subscribe({
+              next: (result) => this.employee = result,
+              error: (err) => this.errorMessage = `Error occured (${err.status})`
+            })
+          }
+        });
+    }
 
   onSubmit() : void {
-    this.employeeService.createEmployee(this.employee)
-    .subscribe({
-      next: (response) => {
-        this.router.navigate(['/']);
-      },
-      error: (err) => {
-        console.error(err);
-        this.errorMessage = `Error occured (${err.status})`;
-      }
-    });
+
+    if(this.isEditing){
+      this.employeeService.editEmployee(this.employee)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error(err);
+          this.errorMessage = `Error occured during updating (${err.status})`;
+        }
+      });
+    } else {
+      // creating
+      this.employeeService.createEmployee(this.employee)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error(err);
+          this.errorMessage = `Error occured during creating (${err.status})`;
+        }
+      });
+    }    
   }
+
 
 }
